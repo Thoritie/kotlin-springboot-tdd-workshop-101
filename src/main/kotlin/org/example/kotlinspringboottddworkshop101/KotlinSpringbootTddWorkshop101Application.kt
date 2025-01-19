5891c7d9a2f4764ccf85637a908afc02f2bd3392
Thoritie <thor@odds.team>
2025-01-19 12:16:57 +0700

    :tada: Intial workshop :snowman:

---
+++ src/main/kotlin/org/example/kotlinspringboottddworkshop101/KotlinSpringbootTddWorkshop101Application.kt
@@ -0,0 +1,11 @@
+package org.example.kotlinspringboottddworkshop101
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringbootTddWorkshop101Application
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringbootTddWorkshop101Application>(*args)
+}
---
+++ src/test/kotlin/org/example/kotlinspringboottddworkshop101/KotlinSpringbootTddWorkshop101ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.kotlinspringboottddworkshop101
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootTddWorkshop101ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
